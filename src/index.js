081b223f76be82d8b0653e83dbf8bd4ea2f4a8ae
Cas du Plessis <go4cas@gmail.com>
2020-08-23 15:41:51 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { registerComponents } from './utils.js'
+import './store.js'
+
+;(() => {
+  registerComponents({
+  })
+  
+  import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.2.x/dist/index.js')
+  import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
+})()
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import Spruce from 'https://cdn.jsdelivr.net/gh/ryangjchandler/spruce@0.x.x/dist/spruce.module.js'
+
+Spruce.config({
+  globalStoreVariable: true
+})
+
+Spruce.store()
+
+export default Spruce
