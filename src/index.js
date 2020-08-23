928a33f2395fe24a97daa82f5dbc620479ace554
Cas du Plessis <go4cas@gmail.com>
2020-08-23 22:04:09 +0200

    Async/await for component registration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { registerComponents } from './utils.js'
 import './store.js'
 
-;(() => {
-  registerComponents({
+;(async () => {
+  await registerComponents({
     toggle: './components/toggle.js'
   })
   
-  import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
-  import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
+  await import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
+  await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
 })()
