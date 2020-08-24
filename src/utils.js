2fb8afa2af93c701ae1845a492cd9593bb2f727e
Cas du Plessis <go4cas@gmail.com>
2020-08-24 11:42:29 +0200

    Remove all async/await

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,8 @@ export const buildComponent = (data, methods = {}, init = () => {}) => {
   return () => { return { init, ...data, ...methods } }
 }
 
-export const registerComponents = async components => {
-  await Object.entries(components).filter(function ([component]) {
+export const registerComponents = components => {
+  Object.entries(components).filter(function ([component]) {
     return document.querySelector(`[x-data="${component}()"]`)
   }).forEach(function ([component, handler]) {
     import(handler).then(module => window[component] = module.default)
