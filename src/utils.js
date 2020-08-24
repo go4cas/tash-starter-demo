a28dbdc11d08cf998c52be99633309e83c811525
Cas du Plessis <go4cas@gmail.com>
2020-08-24 15:25:16 +0200

    Remove async for register function

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ export const buildComponent = (data, methods = {}, init = () => {}) => {
 export const registerComponents = components => {
   Object.entries(components).filter(function ([component]) {
     return document.querySelector(`[x-data="${component}()"]`)
-  }).forEach(async function ([component, handler]) {
-    await import(handler).then(async module => window[component] = await module.default).catch(error => console.error(error))
+  }).forEach(function ([component, handler]) {
+    import(handler).then(module => window[component] = module.default).catch(error => console.error(error))
   })
 }
