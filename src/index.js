b644bf0254eb7ae8135c1c059e728bdb2d583b44
Cas du Plessis <go4cas@gmail.com>
2020-08-24 12:16:52 +0200

    Log all steps

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import { registerComponents } from './utils.js'
 import './store.js'
 
 ;(() => {
+  console.log('registering components...')
   registerComponents({
     toggle: './components/toggle.js'
   })
   
+  console.log('importing magic helpers script...')
   import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
+  console.log('importing alpine script...')
   import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
 })()
--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,10 @@ export const buildComponent = (data, methods = {}, init = () => {}) => {
 
 export const registerComponents = components => {
   Object.entries(components).filter(function ([component]) {
+    console.log('get list of components ...')
     return document.querySelector(`[x-data="${component}()"]`)
   }).forEach(function ([component, handler]) {
+    console.log('create component modules ...')
     import(handler).then(module => window[component] = module.default)
   })
 }
