7e5a94c78d15048105c9ef61859214b07bb716d6
Cas du Plessis <go4cas@gmail.com>
2020-08-24 15:22:25 +0200

    Async for register function

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,6 @@ export const registerComponents = components => {
   Object.entries(components).filter(function ([component]) {
     return document.querySelector(`[x-data="${component}()"]`)
   }).forEach(async function ([component, handler]) {
-    await import(handler).then(module => window[component] = module.default).catch(error => console.error(error))
+    await import(handler).then(async module => window[component] = await module.default).catch(error => console.error(error))
   })
 }
