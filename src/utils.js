5d15e7c247ec18f97a65a7c7794b842e87e2cbbd
Cas du Plessis <go4cas@gmail.com>
2020-08-24 14:01:02 +0200

    Exception handling in registration

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,6 @@ export const registerComponents = components => {
   Object.entries(components).filter(function ([component]) {
     return document.querySelector(`[x-data="${component}()"]`)
   }).forEach(function ([component, handler]) {
-    import(handler).then(module => window[component] = module.default)
+    import(handler).then(module => window[component] = module.default).catch(error => console.error(error))
   })
 }
