b5d06d979c827736457dc18957891b302f5e2a0b
Cas du Plessis <go4cas@gmail.com>
2020-08-25 09:21:13 +0200

    Add terser plugin back into build

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ import './store.js'
   setTimeout(async () => {
     await import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
     await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')  
-  }, 2000)  
+  }, 1000)  
 })()
