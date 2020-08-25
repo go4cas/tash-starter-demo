84ab028bfe351eec2cd75cd0b446230a70103a46
Cas du Plessis <go4cas@gmail.com>
2020-08-25 09:05:39 +0200

    Toggle module cleanup

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,20 +1,20 @@
 import { buildComponent } from '../utils.js'
 
 const data = {
-    open: false,
+  open: false,
 }
 
 const methods = {
-    show() {
-        this.open = true
-    },
-    hide() {
-        this.open = false
-    }
+  show() {
+    this.open = true
+  },
+  hide() {
+    this.open = false
+  }
 }
 
 const init = (() => {
-    console.log('toggle is here!!')
+  console.log('toggle is here!!')
 })
 
 export default buildComponent(data, methods, init)
