63b97f98a2e16a85512b06c27f0f20162f1c5314
Cas du Plessis <go4cas@gmail.com>
2020-08-25 09:19:38 +0200

    Drop timeout to 2 seconds

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ import './store.js'
     toggle: './components/toggle.js'
   })
 
-  setTimeout(function() {
-    import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
-    import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')  
-  }, 10000)  
+  setTimeout(async () => {
+    await import('https://cdn.jsdelivr.net/gh/kevinbatdorf/alpine-magic-helpers@0.3.x/dist/index.js')
+    await import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')  
+  }, 2000)  
 })()
