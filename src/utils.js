036e18531a012e747d49ce546a02c9bc5ebeb5ea
Cas du Plessis <go4cas@gmail.com>
2020-08-25 08:56:28 +0200

    Change build component to normal function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const buildComponent = (data, methods = {}, init = () => {}) => {
-  return () => { return { init, ...data, ...methods } }
+  return function () { return { init, ...data, ...methods } }
 }
 
 export const registerComponents = components => {
